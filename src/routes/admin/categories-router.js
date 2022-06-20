2332dbf1bfa4bf93cb72ed849027ab5268be958e
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-06-20 21:48:39 +0700

    sprint1 - admin CRUD for product and category

---
+++ src/routes/admin/categories-router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const { adminCategoryController } = require("../../controllers");
+
+router.get("/", adminCategoryController.readCategories);
+router.get("/:categoryId", adminCategoryController.readCategoryById);
+router.post("/create", adminCategoryController.createCategory);
+router.delete("/:categoryId/delete", adminCategoryController.deleteCategory);
+router.patch("/:categoryId/update", adminCategoryController.updateCategory);
+
+module.exports = router;
