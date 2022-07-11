efa1dff137879d79df298088dae5fe6caea9d0e9
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-07-11 21:59:25 +0700

    merge sprint 3

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,6 @@
 const adminProductController = require("./admin/product-controller");
 const adminCategoryController = require("./admin/category-controller");
-const adminTransactionContoller = require("./admin/transaction-controller")
+const adminTransactionContoller = require("./admin/transaction-controller");
 const user = require("../controllers/user/user-controller");
 const userProductController = require("./user/product-controller");
 const userCartController = require("./user/cart-controller");
