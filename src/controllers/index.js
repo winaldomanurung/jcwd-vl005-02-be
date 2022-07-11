4643118044b4d604d7c849de790f039bd1d71af4
winaldomanurung <46890502+winaldomanurung@users.noreply.github.com>
2022-07-11 21:44:17 +0700

    Update repo sprint 4 (#5)
    * Sprint-2 Add user registration feature
    
    * Sprint-3 Add Admin transaction feature
    
    Co-authored-by: Vincent Wardhana <39871348+vincentwardhana33@users.noreply.github.com>
    Co-authored-by: addumairi <azharaddumairi@gmail.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const adminProductController = require("./admin/product-controller");
 const adminCategoryController = require("./admin/category-controller");
+const adminTransactionContoller = require("./admin/transaction-controller")
 const user = require("../controllers/user/user-controller");
 const userProductController = require("./user/product-controller");
 const userCartController = require("./user/cart-controller");
@@ -8,6 +9,7 @@ const userCartController = require("./user/cart-controller");
 module.exports = {
   adminProductController,
   adminCategoryController,
+  adminTransactionContoller,
   user,
   userProductController,
   userCartController,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const adminProductRouter = require("./admin/products-router");
 const adminCategoriesRouter = require("./admin/categories-router");
+const adminTransactionRouter = require("./admin/transactions-router")
 const user_router = require("./user/user-routers");
 const userProductRouter = require("./user/products-router");
 const userCartRouter = require("./user/cart-router");
@@ -8,6 +9,7 @@ const userCartRouter = require("./user/cart-router");
 module.exports = {
   adminProductRouter,
   adminCategoriesRouter,
+  adminTransactionRouter,
   user_router,
   userProductRouter,
   userCartRouter,
