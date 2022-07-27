34becda2c66f4bbcd055cc44080a44d6596ea6ed
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-07-27 13:25:09 +0700

    trial websocket 3

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ app.use(
     exposedHeaders: ["UID", "Auth-Token", "Auth-Token-Admin", "Authorization"],
   })
 );
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.static("public"));
 app.use(bearerToken());
 
